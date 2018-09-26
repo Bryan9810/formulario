1a9d6ec581cd187cac88def5ff51a0bd5a259a00
Bryan Rodriguez <bryanrodriguez@Bryans-iMac.local>
2018-09-26 17:42:13 -0500

    formulario condiciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+declare var jQuery:any;
+declare var $:any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formulario';
+  first_name:any = '';
+  last_name:any = '';
+  company:any = '';
+  mail:any = '';
+
+  mysend() {
+    let objeto: any = []
+    objeto.nombre = this.first_name
+    objeto.apellido = this.last_name
+    objeto.empresa = this.company
+    objeto.correo = this.mail
+    console.log(objeto);
+    $('form').trigger("reset")
+  }
+
+  myvalidform (){
+    let name = $('#first_name').val()
+    let name1 = $('#last_name').val()
+    let company = $('#company').val()
+    let email = $('#mail').val()
+
+    if (name == " " || name1 == " " || company == " " || email == " " || email.indexOf('@', 0) == -1 || email.indexOf('.', 0) == -1) {
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
 }
